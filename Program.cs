facd37932a34d5b363b2214ae6ac90a3130e5623
Henrique Jordão Baier De Azevedo <45922978810@info.local>
2023-04-06 10:58:43 -0300

    exercicios feitos

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// EXERCICIO 1
+
+// Console.WriteLine($"Digite uma nota entre 0 e 10:");
+// int nota = int.Parse(Console.ReadLine());
+
+
+// while (nota > 10 || nota < 0)
+// {Console.WriteLine($"ATENCAO DIGITE UMA NOTA ENTRE 0 E 10!!!. Digite uma nota entre 0 e 10:");
+// nota = int.Parse(Console.ReadLine());
+    
+// }
+
+
+
+
+
+// EXERCICIO 2 
+
+// Console.WriteLine($"Digite seu nome de usuario:");
+// string user = Console.ReadLine();
+
+// Console.WriteLine($"Digite sua senha:");
+// string senha = Console.ReadLine();
+
+
+
+// while (senha == user)
+// {Console.WriteLine($"Digite uma senha que nao seja seu nome de usuario.");
+
+// Console.WriteLine($"Digite sua senha:");
+// senha = Console.ReadLine();
+// }
+
+
+
+// EXERCICIO 3 
+// int tabuada = 0;
+
+// Console.WriteLine($"Digite um numero da classe dos inteiros:");
+// int numint = int.Parse(Console.ReadLine());
+
+// for (tabuada = 0; tabuada <= 10; tabuada++)
+//  {int valor = (numint * tabuada);
+  
+//     Console.WriteLine($"{numint} x {tabuada} = {valor}");
+//     }
+
+
+
